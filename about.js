81c30c9ee2fc7b171bd65de2f86ec63449e020ba
dnstrach <dnstrach@gmail.com>
2022-11-04 11:53:52 -0700

    submit

---
+++ about.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form submited');
+}
+
+const img = document.querySelector();
+img.addEventListener('mouseover', () => {
+	alert('Happy Friday!')
+})
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
---
+++ profile.js
@@ -0,0 +1,20 @@
+const colorButton = document.querySelector('#color');
+
+colorButton.addEventListener('click', () => {
+    alert("My favorite color is blue")
+})
+favButtons.appendChild(colorButton)
+
+const placeButton = document.querySelector('#place');
+
+placeButton.addEventListener('click', () => {
+    alert("My favorite place is the ocean")
+})
+favButtons.appendChild(placeButton)
+
+const ritualButton = document.querySelector('#ritual');
+
+ritualButton.addEventListener('click', () => {
+    alert("My favorite ritual is meditating in the sauna")
+})
+favButtons.appendChild(ritualButton)
